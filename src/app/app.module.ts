461a203df149cc7affc4419c45bff44f3e4eea3c
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-08-19 22:59:28 +0100

    first commit front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule,
