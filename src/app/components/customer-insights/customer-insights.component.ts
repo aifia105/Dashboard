c5824a42d1f899bc301a4208c26758f80edad67e
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-10-04 02:02:18 +0100

    clientlist/view

--- src/app/components/customer-insights/customer-insights.component.ts
+++ src/app/components/customer-insights/customer-insights.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-insights.component.scss']
 })
 export class CustomerInsightsComponent {
+  showList : boolean = false;
+
+  toggleList() {
+    this.showList = !this.showList;
+  }
+
+  client = {
+    name :"ahmed ali",
+    birthday : "1/6/1990",
+    adresse : "bouselm",
+    email :  "ahmed@gmail.com",
+    phone : "phone"
+  }
 
 }
